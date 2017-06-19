88e3ae5731d913f7ac24f4f9a7b3bd461fc038b7
mrsmith01 <aseenyur@gmail.com>
2017-06-19 15:08:06 -0400

    First Commit

---
+++ media.py
@@ -0,0 +1,14 @@
+# allows access to webbrowser function in python library
+import webbrowser
+
+# defines movie class and data structure
+class Movie():
+    def __init__(self, movie_title,movie_storyline,poster_image,trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+# calls python webbrower function to open movie trailer in web browser
+    def show_trailer (self):
+        webbrowser.open(self.trailer_youtube_url)
---
+++ readME.md
@@ -0,0 +1,18 @@
+# Udacity_Movie_Project
+
+Udacity FSND movie trailer website - project 1
+
+### Included files
+
+- fresh_tomatoes.html
+- entertainment_center.py
+- media.py
+- fresh.tomatoes.py
+
+### Live Demo - Movie trailer website will load in default browser
+
+- [Movie Trailer Link](https://github.com/mrsmith01/udacity_movie_project/blob/master/fresh_tomatoes.html)
+
+#### Downloads
+
+- [Project files]()
