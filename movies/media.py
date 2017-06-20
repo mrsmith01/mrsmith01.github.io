1e98f34b7f005fa962910495145dc684e74166e3
mrsmith01 <aseenyur@gmail.com>
2017-06-20 13:48:14 -0400

    update media file (docstring)

--- movies/media.py
+++ movies/media.py
@@ -1,7 +1,7 @@
-# allows access to webbrowser function in python library
+""" allows access to webbrowser function in python library"""
 import webbrowser
 
-# defines movie class and data structure
+"""defines movie class and data structure"""
 class Movie():
     def __init__(self, movie_title,movie_storyline,poster_image,trailer_youtube):
         self.title = movie_title
@@ -9,6 +9,6 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-# calls python webbrower function to open movie trailer in web browser
+"""calls python webbrower function to open movie trailer in web browser"""
     def show_trailer (self):
         webbrowser.open(self.trailer_youtube_url)
